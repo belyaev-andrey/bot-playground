206c3ff2d8cafc96f974f0b1395167efac13933a
belyaev-andrey <belyaev.andrey@gmail.com>
2021-12-22 17:15:41 +0300

    Test changes

--- src/main/java/org/acme/test/TestApplication.java
+++ src/main/java/org/acme/test/TestApplication.java
@@ -3,6 +3,6 @@ package org.acme.test;
 public class TestApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world1");
     }
 }
