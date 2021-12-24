97d561f3fa6407915481aa3edd0b85307efd0fff
belyaev-andrey <belyaev.andrey@gmail.com>
2021-12-24 11:46:24 +0300

    Added wrong entity

--- src/main/java/org/acme/test/TestApplication.java
+++ src/main/java/org/acme/test/TestApplication.java
@@ -1,12 +1,24 @@
 package org.acme.test;
 
-public class TestApplication {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         System.out.println("Hello world");
         String s = "10";
         if (s.equals("1"+"0")) {
             System.out.println("Hello world");
         }
     }
+
 }
